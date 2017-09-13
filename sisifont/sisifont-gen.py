d681b2e0e9da88ab6c696a96142834ad41149049
Axolotle <nchesnais@mailoo.org>
2017-09-13 14:34:27 +0200

    Added sisifont python generator

---
+++ sisifont/sisifont-gen.py
@@ -0,0 +1,23 @@
+txt = "TEST"
+
+
+font = []
+with open("sisifontFile.txt", "r") as f:
+    for i, line in enumerate(f):
+        if i >= 2 and i <= 5:
+            font.append(line)
+
+output = ["", "", "", ""]
+for c in txt:
+    c = (ord(c)-32)*3
+    output_i = 0
+    for l in font:
+        output[output_i] += l[c:c+3]
+        output_i += 1
+
+with open("test.txt", "w") as output_file:
+    for line in output:
+        output_file.write(line+ "\n")
+
+for line in output:
+    print(line)
