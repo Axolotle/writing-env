81ad65ee8165fca3e239df09b3c2491e4e47704f
Axolotle <nchesnais@mailoo.org>
2017-09-14 16:21:13 +0200

    added childnodes removal & weird spaces

--- sisifont/sisifont-gen.js
+++ sisifont/sisifont-gen.js
@@ -2,8 +2,31 @@
 var button = document.getElementById('gen');
 button.addEventListener("click", generateFont);
 
+function getPureText(nodes) {
+    var sentences = [];
 
-function generateFont() {
+    for (var i = 0; i < nodes.length; i++) {
+        let sentence = "";
+
+        for (var child = 0; child < nodes[i].childNodes.length; child++) {
+            if (nodes[i].childNodes[child].nodeName != "#text" ) {
+                sentence += nodes[i].childNodes[child].innerHTML;
+            }
+            else {
+                sentence += nodes[i].childNodes[child].data;
+            }
+        }
+
+        sentence = sentence.replace(/&nbsp;/g, " ").replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/<br>/g, "");
+
+        sentences.push(sentence);
+    }
+
+    return sentences;
+}
+
+function textToFont(txts) {
+
     var font = [
         "                                                                                                                                                                                                                                                                                                                                                                                                                           ╭─╮            ╭─╮╶─╴ ○    ╶╮ ╶╮                 ╮ ╭╮                 \\  /  ^  ~ · · ○        \\  /  ^ · · \\  /  ^ · ·    ~  \\  /  ^  ~ · ·       \\  /  ^ · · /                                                                                                       ",
         "    ╷  ││ ┼┼╭┼╴ ○ ╭╮  │  ╭╯╰╮ ┬┴┬              /╭─╮╶╮ ╶─╮╶─╮╷  ┌─╴╭─╴╶─┐╭─╮╭─╮               ╶─╮╭─╮╭─┐┌─╮╭─╴┌─╮┌─╴┌─╴╭─╮╷ ╷╶┬╴  ╷╷ ╭╷  ╭╮╮╭╮╷╭─╮┌─╮╭─╮┌─╮╭─╴╶┬╴╷ ╷╷ ╷╷╷╷╷ ╷╷ ╷╶─╮ ┌╴\\  ╶┐  ^     \\    ╷       ╷    ╭╴   ╷   ╷  ╷ ╷  ╶┐                       ╷                    ╭╯ │ ╰╮                                                                                                                 ╭╮ ╷ ╷ ╷ ╷ ╭─╮· ·│╭│╶╮          │╭│         ╭╯  ┤  ╯    ╭┬┬       │ ││    ┌┬┐┌─┐┌─┐   ╭─┐╭─┐╭─┐╭─┐╭─┐╭─┐╭┬╴╭─╴┌─╴┌─╴┌─╴┌─╴╶┬╴╶┬╴╶┬╴╶┬╴┌─╮╭╮╷╭─╮╭─╮╭─╮╭─╮╭─╮   ╭─/╷ ╷╷ ╷╷ ╷╷ ╷╷ ╷├─╮┌─╮ \\  /  ^  ~ · · ○        \\  /  ^ · · \\  /  ^ · · ╶┤ ~  \\  /  ^  ~ · · ·     \\  /  ^ · · /    · ·",
@@ -12,34 +35,37 @@ function generateFont() {
         "                                                                                                                                                                                                                     ╶─╯      ╶╯                ╵    ╵                     ╶─╯                                                                                                                      ╵              ╵ ╰─╯                                       ╵                             ╰─╴                      ╯                                                                                               ╯                                                                ╶─╯   ╶─╯"
     ];
 
+    var sentences = [];
+    txts.forEach(function(txt) {
+        var sentence = ["","","","",""];
 
-    var input = document.getElementById('input').firstChild.innerHTML;
-    console.log("/"+input+"/");
-    input = input.replace(/&nbsp;/g, " ");
-    input = input.replace(/&amp;/g, "&");
-    input = input.replace(/&lt;/g, "<");
-    input = input.replace(/&gt;/g, ">");
+        for (var c = 0; c < txt.length; c++) {
+            if (txt.charCodeAt(c) == 10) {
+                continue;
+            }
+            var pos = (txt.charCodeAt(c)-32)*3;
 
-    console.log(input.indexOf("&nbsp;"));
-    var output = ["", "", "", "", ""];
-
-    for (var c = 0; c < input.length; c++) {
-        var pos = (input.charCodeAt(c)-32)*3;
-
-        for (var i = 0; i < font.length; i++) {
-            output[i] += font[i][pos]+font[i][pos+1]+font[i][pos+2];
+            for (var i = 0; i < font.length; i++) {
+                sentence[i] += font[i][pos]+font[i][pos+1]+font[i][pos+2];
+            }
         }
-    }
+        sentences.push(sentence);
+    });
 
-    display(output);
+    return sentences;
 }
 
-function display(strings) {
+function generateFont() {
+    var input = textToFont(getPureText(document.getElementById('input').children));
+
     var div = document.getElementById('displayer');
 
-    for (var i = 0; i < strings.length; i++) {
-        let p = document.createElement("P");
-        p.innerHTML = strings[i];
-        div.appendChild(p);
-    }
+    input.forEach(function(sentence) {
+        sentence.forEach(function(line) {
+            let p = document.createElement("P");
+            p.innerHTML = line;
+            div.appendChild(p);
+        });
+    });
+
 }
