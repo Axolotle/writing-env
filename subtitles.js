561191b23f6acf033c1d4454fbc03bb8b99765f3
Axolotle <nchesnais@mailoo.org>
2017-10-24 13:45:44 +0200

    updating script

--- subtitles.js
+++ subtitles.js
@@ -1,131 +1,133 @@
 var box = new Box();
+var file = "luvan";
 
-readTxtFile("test.txt", parseSubtitles, runSubtitles);
+readTxtFile(file)
+.then(txtFile => parseSubtitles(txtFile))
+.then(subtitles => runSubtitles(subtitles));
 
 document.getElementById("format").addEventListener("click", function() {
-    readTxtFile("test.txt", parseSubtitles, displayJSON);
+    readTxtFile(file)
+    .then(txtFile => parseSubtitles(txtFile))
+    .then(subtitles => displayJSON(subtitles));
 });
 
 function readTxtFile(file) {
     return new Promise((resolve, reject) => {
         var rawFile = new XMLHttpRequest();
-        rawFile.open("GET", file);
+        rawFile.open("GET", file + ".txt");
         rawFile.overrideMimeType("text/plain");
         rawFile.onload = () => {
             if (rawFile.readyState === 4 && rawFile.status == "200") {
                 resolve(rawFile.responseText);
             }
         }
-        rawFile.onerror = () => reject("Couldn't find '" + url +"'");
+        rawFile.onerror = () => reject("Couldn't find '" + file +"'");
         rawFile.send(null);
     });
 }
 
-function parseSubtitles(data, callback) {
-    if (data.indexOf("<--") > -1) {
-        data = data.substring(data.indexOf("<--")+5);
-    }
-    if (data.indexOf("-->") > -1) {
-        data = data.substring(0, data.indexOf("-->")-2);
-    }
-    if (data[data.length-1] == " " || data[data.length-1] == "\n") {
-        data = data.substring(0, data.length-1);
-    }
-
-    data = data.split('\n\n');
-
-    var txt = [];
-    var sumTimer = 0;
-
-    data.forEach(function(sub) {
-        sub = sub.split("\n");
-
-        var opt = sub.shift().split(" ");
-
-        // formatting timer
-        var timer = [];
-        sumTimer += parseFloat(opt[0]) *1000;
-        timer.push(sumTimer);
-        sumTimer += parseFloat(opt[1]) *1000
-        timer.push(sumTimer);
-
-        var color, pos;
-
-        // FIXME 5 a la place de 4
-        if (opt.length == 5 || opt.length == 4) {
-            color = opt[2];
-            pos = opt[3].split("/").map(function(n) {return parseInt(n)});
+function parseSubtitles(data) {
+    return new Promise((resolve, reject) => {
+        if (data.indexOf("<--") > -1) {
+            data = data.substring(data.indexOf("<--")+5);
         }
-        else if (opt.length == 3) {
-            if (isNaN(parseInt(opt[2][0]))) {
+        if (data.indexOf("-->") > -1) {
+            data = data.substring(0, data.indexOf("-->")-2);
+        }
+        if (data[data.length-1] == " " || data[data.length-1] == "\n") {
+            data = data.substring(0, data.length-1);
+        }
+
+        data = data.split('\n\n');
+
+        var txt = [];
+        var sumTimer = 0;
+
+        data.forEach(function(sub) {
+            sub = sub.split("\n");
+
+            var opt = sub.shift().split(" ");
+
+            // formatting timer
+            var timer = [];
+            sumTimer += parseFloat(opt[0]) *1000;
+            timer.push(sumTimer);
+            sumTimer += parseFloat(opt[1]) *1000
+            timer.push(sumTimer);
+
+            var color, pos;
+
+            // FIXME 5 a la place de 4
+            if (opt.length == 5 || opt.length == 4) {
                 color = opt[2];
-                pos = [50,100];
+                pos = opt[3].split("/").map(function(n) {return parseInt(n)});
+            }
+            else if (opt.length == 3) {
+                if (isNaN(parseInt(opt[2][0]))) {
+                    color = opt[2];
+                    pos = [50,100];
+                }
+                else {
+                    color = "face";
+                    pos = opt[2].split("/").map(function(n) {return parseInt(n)});
+                }
             }
             else {
                 color = "face";
-                pos = opt[2].split("/").map(function(n) {return parseInt(n)});
+                pos = [50,100];
             }
-        }
-        else {
-            color = "face";
-            pos = [50,100];
-        }
-        //FIXME /!\ opt[4] a l'arrache
-        lol = [timer, sub, color, pos];
-        if (opt[4] != undefined) lol.push(opt[4])
-        txt.push(lol);
-    });
+            //FIXME /!\ opt[4] a l'arrache
+            lol = [timer, sub, color, pos];
+            if (opt[4] != undefined) lol.push(parseInt(opt[4]))
+            txt.push(lol);
+        });
 
-    //callback(txt);
-    // FIXME
-    displayJSON(txt);
+        resolve(txt);
+    });
 }
 
-function runSubtitles(txt) {
+async function runSubtitles(txt) {
     var opt = {
-        "longestWord": 14,
-        "divName": "text",
-        "idealX": 99,
-        "idealY": 32,
-        "marginX": 2,
-        "marginY": 2
+        divName: "text",
+        maxY: 37,
+        minY: 27,
+        marginX: 2,
+        marginY: 2,
+        ratio: [16,9]
     };
 
-    var txts = [
-        {
-            "txt": [
-                " /                            ",
-                "┌─╴┌─╮╶┬╴╭─╴╭─╮┌─╮┌─╴   ╶─╮╭─┐",
-                "├─╴├─╯ │ ╰─╮│ ││ │├─╴    ─┤├─┤",
-                "╰─╴╵  ╶┴╴╶─╯╰─╯└─╯╰─╴   ╶─╯╵ ╵",
-                "~ 0 min 0 sec",
-                "",
-                "pas d'interaction",
-                "",
-                "",
-                "<a id='start' class='ep'>Lancer l'épisode</a>"
-            ],
-            "charToAdd": " "
-        },
+    var menu = {
+        txt: [
+            " /                            ",
+            "┌─╴┌─╮╶┬╴╭─╴╭─╮┌─╮┌─╴   ╶─╮╭─┐",
+            "├─╴├─╯ │ ╰─╮│ ││ │├─╴    ─┤├─┤",
+            "╰─╴╵  ╶┴╴╶─╯╰─╯└─╯╰─╴   ╶─╯╵ ╵",
+            "~ 0 min 0 sec",
+            "",
+            "pas d'interaction",
+            "",
+            "",
+            "{{tag::a|id=start|class=ep}}Lancer l'épisode{{tag::/a}}"
+        ],
+        format: "align",
+        charToAdd: " "
+    };
 
-        {
-            "txt": [],
-            "format": "subtitle"
-        }
-    ];
+    var sub = {
+        txt: txt,
+        format: "subtitle"
+    };
 
-    txts[1].txt = txt
-
-    box.init(boxOptions);
+    box.init(opt);
     const formatter = new FormatJSON(box.x, box.y, box.marginX, box.marginY);
-    const infos = new Animation(formatter.getNewJSON(txts.shift()));
-    const sub = new Animation(formatter.getNewJSON(txts));
+    infos = new Animation(formatter.getNewJSON(menu));
+    sub = new Animation(formatter.getNewJSON(sub));
 
     box.display(box);
-    infos.appendText(box);
+    infos.displayText(box);
 
     await startListener();
-    await sub[0].startSubtitles(box);
+    await sub.startSubtitles(box);
 }
 
 function displayJSON(txt) {
@@ -134,7 +136,6 @@ function displayJSON(txt) {
     if (document.getElementById("center")) {
         x.removeChild(document.getElementById("center"));
     }
-
 
     var structure = '{<br>&Tab;"txt": [<br>';
 
@@ -174,15 +175,14 @@ function displayJSON(txt) {
     else {
         document.getElementById("code").innerHTML = structure;
     }
-
-
 }
 
-
-function startEp(callback) {
-    var link = document.getElementById("start");
-    link.addEventListener("click", function() {
-        box.cleanBox();
-        callback();
-    })
+function startListener() {
+    return new Promise ((resolve, reject) => {
+        var link = document.getElementById("start");
+        link.addEventListener("click", () => {
+            box.cleanLines();
+            resolve();
+        });
+    });
 }
